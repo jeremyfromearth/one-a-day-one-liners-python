202264f103fb5c485303e7ee021f3576b3a48d37
jeremyfromearth <jeremynealbrown@gmail.com>
2023-01-03 21:43:54 +0100

    Adds One Liner 2023.01.02

---
+++ 2023.01.02.py
@@ -0,0 +1,11 @@
+print('One a Day One Liners - 2023.01.02')
+print('Divide and floor!!\n')
+
+if __name__ == '__main__':
+  print('i in range(0, 100, 7) divided by 4')
+  for i in range(0, 100, 7):
+    print(f'i: {i}, i // 4 = {i // 4}')
+
+  print('i in range(0, 100, 19) divided by 3')
+  for i in range(0, 100, 19):
+    print(f'i: {i}, i // 3 = {i // 3}')

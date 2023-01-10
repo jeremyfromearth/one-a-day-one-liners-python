b9e8a62fb299b4404890b5678886f0d1b08e875b
jeremyfromearth <jeremynealbrown@gmail.com>
2023-01-10 09:35:35 +0100

    Updates comment

--- 2023.01.10.py
+++ 2023.01.10.py
@@ -6,7 +6,7 @@ import re
 with open('./data/text/names.txt') as f:
   stop = f.read().split('\n')
 
-# Remove all names from the article
+# Redact all names from the article
 with open('./data/text/wired-brains-perception.txt') as f:
   text = f.read().replace('\n', ' ')[:-1]
   redacted = ' '.join([t if t not in stop else '*' * len(t) for t in re.sub(r'[^\w\s]', '', text).split(' ')])

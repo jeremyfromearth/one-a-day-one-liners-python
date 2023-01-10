2fb18948177a1f74161b0c69e7ef2692f3c51252
jeremyfromearth <jeremynealbrown@gmail.com>
2023-01-10 09:37:51 +0100

    Cleaned up 2023.01.10 a bit

--- 2023.01.10.py
+++ 2023.01.10.py
@@ -4,10 +4,10 @@ print('Redact a top secret document 🕵')
 import re
 
 with open('./data/text/names.txt') as f:
-  stop = f.read().split('\n')
+  names = f.read().split('\n')
 
-# Redact all names from the article
 with open('./data/text/wired-brains-perception.txt') as f:
-  text = f.read().replace('\n', ' ')[:-1]
-  redacted = ' '.join([t if t not in stop else '*' * len(t) for t in re.sub(r'[^\w\s]', '', text).split(' ')])
+  text = f.read()
+  # Redact names from the article
+  redacted = ' '.join([t if t not in names else '*' * len(t) for t in re.sub(r'[^\w\s]', '', text).split(' ')])
   print(redacted)

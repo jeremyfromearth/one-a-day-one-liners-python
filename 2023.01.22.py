43252833d4a44813982acf6fcf657feca781de2d
jeremyfromearth <jeremynealbrown@gmail.com>
2023-01-22 15:25:03 +0100

    Adds 2023.01.22 and updats gitignore

---
+++ 2023.01.22.py
@@ -0,0 +1,46 @@
+print('One a Day One Liners - 2023.01.22')
+print('Apply a function over each line of a file 📄')
+
+from collections import Counter
+from datetime import datetime
+
+line_counter = 0
+kw_counter = Counter()
+FILE = './data/csv/web-of-science-20k.csv'
+
+
+def count_keywords(line: str) -> None:
+  """
+    Count keywords from a give line
+  """
+  global line_counter
+  line_counter += 1
+  cols = line.split(',')
+  if cols[0] == '0':
+    kws = filter(
+      lambda x: len(x),
+      map(lambda x: x.strip(), cols[5].split(' ')))
+
+    kw_counter.update(kws)
+
+
+# Apply the count keywords function line by line
+t1 = datetime.now()
+for line in open(FILE): count_keywords(line)
+t2 = datetime.now()
+
+print(f'Parsed keywords from {line_counter} lines in {t2-t1}')
+for kw in kw_counter.most_common(32):
+  print(kw)
+
+kw_counter = Counter()
+t3 = datetime.now()
+with open(FILE) as f:
+  lines = f.read().split('\n')
+  for line in lines:
+    count_keywords(line)
+t4 = datetime.now()
+
+print(f'Parsed keywords after reading whole file in {t4-t3}')
+for kw in kw_counter.most_common(32):
+  print(kw)

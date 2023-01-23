caae701d9004f50e537dedf9f8eeca8315118d51
jeremyfromearth <jeremynealbrown@gmail.com>
2023-01-23 07:11:04 +0100

    Adds 2023.01.32

---
+++ 2023.01.23.py
@@ -0,0 +1,15 @@
+print('One a Day One Liners - 2023.01.22')
+print('Create a Timed Rotating Log File 🪵')
+
+import logging
+import time
+from logging.handlers import TimedRotatingFileHandler as TRF
+
+
+logger = logging.getLogger()
+logger.addHandler(TRF('./logs/one-liners.log', when='S', interval=5, backupCount=10))
+
+for i in range(32):
+  print(f'Logging message {i} - check ./logs dir')
+  logger.warning(f'Log message {i}')
+  time.sleep(0.5)
